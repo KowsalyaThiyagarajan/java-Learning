ade1a179f244c363323e2d02785d4e5e80ef215d
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2021-12-13 19:47:46 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,12 @@
+
+public class Logical {
+
+	public static void main(String[] args) {
+        int a=6;
+		System.out.println(a<5&&a>1);
+		System.out.println(a<5||a<1);
+		System.out.println(!(a<5&&a>1));
+
+	}
+
+}
---
+++ bitwise.java
@@ -0,0 +1,17 @@
+
+public class bitwise {
+
+	public static void main(String[] args) {
+		int a=6;
+		int b=1;
+		
+		System.out.println(a|b);
+		System.out.println(a&b);
+		System.out.println(a^b);
+		System.out.println(a<<b);
+		System.out.println(a>>b);
+		System.out.println(a);
+
+	}
+
+}
