92d2724627053a83de87f7cb4d861dd966ead0da
KowsalyaThiyagarajan <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2021-12-13 10:00:41 +0530

    Add files via upload

---
+++ arthimetic.java
@@ -0,0 +1,16 @@
+
+public class arthimetic {
+
+	public static void main(String[] args) {
+		int a=6;
+		int b=5;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+
+
+	}
+
+}
