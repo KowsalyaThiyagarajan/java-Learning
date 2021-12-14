5194e9bc1e2810a1506a1cc0adac47e9ff92ca0b
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2021-12-14 14:33:32 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,15 @@
+
+public class Relational {
+
+	public static void main(String[] args) {
+		int a=6;
+		int b=7;
+System.out.println(a==b);
+System.out.println(a!=b);
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a>=b);
+System.out.println(a<=b);
+	}
+
+}
---
+++ precedence.java
@@ -0,0 +1,12 @@
+
+public class precedence {
+
+	public static void main(String[] args) {
+		int a=7;
+		int b=8;
+		int result = (8+9*2)*6+2;
+		System.out.println(result);
+
+	}
+
+}
