71212cbf4ac3a51cc159b4b50ef602da739a59bc
KOWSALYA T <91681715+KowsalyaThiyagarajan@users.noreply.github.com>
2021-12-22 20:34:43 +0530

    Add files via upload

---
+++ Assignment7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//Program to Find the Square of a given number(using pow method)//
+public class Assignment7 {
+
+	public static void main(String[] args) {
+
+Scanner sd=new Scanner(System.in);
+System.out.println("Enter the number:");
+int num=sd.nextInt();
+
+System.out.println(Math.pow(num, 2));
+	}
+
+}
---
+++ Assignment7_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Assignment7_2 {
+	public static void main(String[] args) {
+
+		Scanner sd=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num=sd.nextInt();
+		int square=num*num;
+		System.out.println("Square of a number is:"+square);
+}
+}
